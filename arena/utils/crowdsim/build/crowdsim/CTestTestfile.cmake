f96727fdbebeae9796c509c14586f6cd9cec28a4
Tim Seeger <tim.seeger@campus.tu-berlin.de>
2024-05-26 15:03:10 +0000

    crowdsim building

---
+++ arena/utils/crowdsim/build/crowdsim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /ros2-migration/arena/utils/crowdsim
+# Build directory: /ros2-migration/arena/utils/crowdsim/build/crowdsim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
