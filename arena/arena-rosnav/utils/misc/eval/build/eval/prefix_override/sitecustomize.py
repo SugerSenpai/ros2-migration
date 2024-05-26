4faf68a47c38f355db17a4a2bb54ac0cfe5024ab
Tim Seeger <tim.seeger@campus.tu-berlin.de>
2024-05-26 15:57:53 +0000

    mapping more like capping

---
+++ arena/arena-rosnav/utils/misc/eval/build/eval/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/ros2-migration/arena/arena-rosnav/utils/misc/eval/install/eval'
---
+++ arena/arena-rosnav/utils/misc/mapping/build/mapping/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /ros2-migration/arena/arena-rosnav/utils/misc/mapping
+# Build directory: /ros2-migration/arena/arena-rosnav/utils/misc/mapping/build/mapping
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
